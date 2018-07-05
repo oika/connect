a5d57625d8fce399ef8ae02ab3cb921dd217ed58
oika <942093+oika@users.noreply.github.com>
2018-07-05 19:19:20 +0900

    Replace get methods with property in JobManagerInfo.py.

---
+++ platform/software/test/test_jobmanagerinfo.py
@@ -0,0 +1,31 @@
+﻿# -*- coding: utf-8 -*-
+
+from JobManagerInfo import JobManagerInfo
+
+import unittest
+
+class TestJobManagerInfo(unittest.TestCase):
+
+    def test_get_properties(self):
+        info = JobManagerInfo("00:00:00:00:00:00", "127.0.0.1", 12345)
+        assert info.mac_addr is "00:00:00:00:00:00"
+        assert info.ip_addr is "127.0.0.1"
+        assert info.port is 12345
+
+    def test_cannot_set_mac_addr(self):
+        with self.assertRaises(AttributeError):
+            info = JobManagerInfo("00:00:00:00:00:00", "127.0.0.1", 12345)
+            info.mac_addr = "0a:0b:0c:0d:0e:0f"
+
+    def test_cannot_set_ip_addr(self):
+        with self.assertRaises(AttributeError):
+            info = JobManagerInfo("00:00:00:00:00:00", "127.0.0.1", 12345)
+            info.ip_addr = "192.168.1.1"
+
+    def test_cannot_set_port(self):
+        with self.assertRaises(AttributeError):
+            info = JobManagerInfo("00:00:00:00:00:00", "127.0.0.1", 12345)
+            info.port = 222
+        
+if __name__=='__main__':
+    unittest.main()
