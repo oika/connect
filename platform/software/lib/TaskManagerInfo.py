a667b4848c779c7938719f8dd3fb33a4ffc58c3c
oika <942093+oika@users.noreply.github.com>
2018-07-06 17:05:55 +0900

    Use property in TaskManagerInfo.py in order to capsule the object.

--- platform/software/lib/TaskManagerInfo.py
+++ platform/software/lib/TaskManagerInfo.py
@@ -7,21 +7,45 @@ class TaskManagerInfo:
 
     def __init__(self, name, manager_mac, manager_address, manager_port,
                  device_type, slots, data_interfaces):
-        self.name = name
-        self.manager_address = manager_address
-        self.manager_mac = manager_mac
-        self.manager_port = manager_port
-        self.device_type = device_type
-        self.slots = slots
-        self.data_interfaces = []
+        self.__name = name
+        self.__manager_address = manager_address
+        self.__manager_mac = manager_mac
+        self.__manager_port = manager_port
+        self.__device_type = device_type
+        self.__slots = slots
+        self.__data_interfaces = []
         for interface in data_interfaces:
             mac_addr = interface['mac']
             ip_addr = interface['address']
             port = str(interface['port'])
-            self.data_interfaces.append(DataInterface(mac_addr, ip_addr, port))
+            self.__data_interfaces.append(DataInterface(mac_addr, ip_addr, port))
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def manager_address(self):
+        return self.__manager_address
+    
+    @property
+    def manager_mac(self):
+        return self.__manager_mac
+    
+    @property
+    def manager_port(self):
+        return self.__manager_port
+    
+    @property
+    def device_type(self):
+        return self.__device_type
+    
+    @property
+    def slots(self):
+        return self.__slots
 
     def reserve_data_interface(self):
-        for interface in self.data_interfaces:
+        for interface in self.__data_interfaces:
             mac_addr = interface.mac_addr
             ip_addr = interface.ip_addr
             port = interface.port
---
+++ platform/software/test/test_taskmanagerinfo.py
@@ -0,0 +1,37 @@
+﻿# -*- coding: utf-8 -*-
+
+from TaskManagerInfo import TaskManagerInfo
+
+import unittest
+
+class TestTaskManagerInfo(unittest.TestCase):
+
+    def test_get_properties(self):
+        info = TaskManagerInfo("foo", "xx:yy:zz", "192.168.1.2", 1234, "CPU", 2, [])
+        self.assertEqual(info.name, "foo")
+        self.assertEqual(info.manager_mac, "xx:yy:zz")
+        self.assertEqual(info.manager_address, "192.168.1.2")
+        self.assertEqual(info.manager_port, 1234)
+        self.assertEqual(info.device_type, "CPU")
+        self.assertEqual(info.slots, 2)
+    
+    def test_reserve_data_interface(self):
+        ifs = [{"address":"a1", "mac":"m1", "port":1111}, {"address":"a2", "mac":"m2", "port":2222}]
+        info = TaskManagerInfo("", "", "", 0, "", 0, ifs)
+
+        resMc, resAd, resPt = info.reserve_data_interface()
+        self.assertEqual(resMc, "m1")
+        self.assertEqual(resAd, "a1")
+        self.assertEqual(resPt, "1111")
+
+        resMc, resAd, resPt = info.reserve_data_interface()
+        self.assertEqual(resMc, "m2")
+        self.assertEqual(resAd, "a2")
+        self.assertEqual(resPt, "2222")
+
+        n = info.reserve_data_interface()
+        self.assertIsNone(n)
+
+    
+if __name__=='__main__':
+    unittest.main()
