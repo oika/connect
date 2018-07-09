3f337f50cf82e9bc672eeae5d2e85ec69877799e
oika <942093+oika@users.noreply.github.com>
2018-07-09 19:46:06 +0900

    Use property in ResourceManagerInfo.py in order to capsule the object.

--- platform/software/lib/ResourceManagerInfo.py
+++ platform/software/lib/ResourceManagerInfo.py
@@ -3,5 +3,13 @@
 class ResourceManagerInfo:
 
     def __init__(self, manager_address, manager_port):
-        self.manager_address = manager_address
-        self.manager_port = manager_port
+        self.__manager_address = manager_address
+        self.__manager_port = manager_port
+    
+    @property
+    def manager_address(self):
+        return self.__manager_address
+    
+    @property
+    def manager_port(self):
+        return self.__manager_port
---
+++ platform/software/test/test_resourcemanagerinfo.py
@@ -0,0 +1,25 @@
+﻿# -*- coding: utf-8 -*-
+
+from ResourceManagerInfo import ResourceManagerInfo
+
+import unittest
+
+class TestResourceManagerInfo(unittest.TestCase):
+
+    def test_get_properties(self):
+        info = ResourceManagerInfo("192.168.1.2", 1234)
+        self.assertEqual(info.manager_address, "192.168.1.2")
+        self.assertEqual(info.manager_port, 1234)
+
+    def test_cannot_set_ip_addr(self):
+        with self.assertRaises(AttributeError):
+            info = ResourceManagerInfo("127.0.0.1", 12345)
+            info.manager_address = "192.168.1.1"
+
+    def test_cannot_set_port(self):
+        with self.assertRaises(AttributeError):
+            info = ResourceManagerInfo("127.0.0.1", 12345)
+            info.manager_port = 222
+    
+if __name__=='__main__':
+    unittest.main()
